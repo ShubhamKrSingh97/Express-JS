71061c8cad9644053640bd91657a98875d692b42
ShubhamKrSingh97 <shubhkrsingh.1997@gmail.com>
2023-04-17 12:30:45 +0530

    parsing input in ExpressJs

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-const http=require('http');
 const express=require('express');
 const app=express();
+const bodyParser=require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}));
 
-app.use((req,res,next)=>{
-    console.log("In a middleware");
-    next();
+app.use('/add-product',(req,res,next)=>{
+    res.send(`<html><h1>Add Product</h1><form action='/product' method="POST"><input type='text' name='title'></input><input type='text' name='size'></input><button type='submit'>Add</button></form></html>`);
 })
 
-app.use((req,res,next)=>{
-    console.log("In another middleware");
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
 })
 
-const server=http.createServer(app);
-server.listen(3000);
+app.use('/',(req,res,next)=>{
+    res.send(`<h1>Welcome to Express Js</h1>`);
+   });
+
+app.listen(3000);
