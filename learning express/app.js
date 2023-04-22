32f1ca96f2d4ac89a90de99c477391a887f1734f
ShubhamKrSingh97 <shubhkrsingh.1997@gmail.com>
2023-04-22 12:26:16 +0530

    Styling and sending static files like CSS

--- learning express/app.js
+++ learning express/app.js
@@ -3,15 +3,21 @@ const app = express();
 //importing custom module
 const adminRoutes = require('./routes/admin');  
 const shopRoutes = require('./routes/shop');
+const contactRoutes=require('./routes/contact');
+const successRoutes=require('./routes/success');
+const path=require('path');
+
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname,'public')));//for sending static files(CSS in this case)
 
 app.use('/admin', adminRoutes); //admin route filtering
 app.use('/shop', shopRoutes); //shop route filtering
-
+app.use(contactRoutes);
+app.use(successRoutes);
 //To handle invalid routes
 app.use((req, res, next) => {
-    res.status(404).send(`<h2>Page Not Found</h2>`);
+    res.status(404).sendFile(path.join(__dirname,'views','error.html'));
 })
 app.listen(3000);
--- learning express/routes/admin.js
+++ learning express/routes/admin.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+const rootDir = require('../util/path')
 
 router.get('/add-product', (req, res, next) => {
-    res.send(`<html><h1>Add Product</h1><form action='/admin/add-product' method="POST"><input type='text' name='title'></input><input type='text' name='size'></input><button type='submit'>Add</button></form></html>`);
-})
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+});
 
 router.post('/add-product', (req, res, next) => {
     console.log(req.body);
